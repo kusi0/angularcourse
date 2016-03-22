4a17c773ac38e288d60f2e4f032a4ca05e7dd731
kusi0 <mi.kusmierczyk@gmail.com>
2016-03-22 17:49:07 +0000

    Final change name

--- app/index.js
+++ app/index.js
@@ -4,7 +4,8 @@ import angular from 'angular';
 import uirouter from 'angular-ui-router';
 import routing from './config';
 
+import home from './features/home';
 
 const ngModule = angular
-        .module('app', [uirouter])
+        .module('app', [uirouter, home])
         .config(routing);
