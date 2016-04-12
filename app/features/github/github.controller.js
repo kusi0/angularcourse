9f15e446041819b7239466212996d4496ceb3c19
kusi0 <mi.kusmierczyk@gmail.com>
2016-04-12 16:44:33 +0000

    Add new directive

--- app/features/github/github.controller.js
+++ app/features/github/github.controller.js
@@ -6,14 +6,23 @@ export default class GitHubController{
     }
     
     makeRequest() {
-        this.$http.get('https://api.github.com/users/bslipek')
+        this.$http.get('https://api.github.com/users/kusi0')
          .then(this.assignToScope.bind(this));
    }
    
     assignToScope(response){
         this.email = response.data.email;
         this.User = response.data;
+        
+        this.$http.get(response.data.repos_url)
+         .then(this.assignReposToScope.bind(this))
+        
         console.log(this.User)
+    }
+    
+    assignReposToScope(response){
+        this.Repos = response.data
+        console.log(this.Repos)
     }
     
 }
--- app/features/github/index.js
+++ app/features/github/index.js
@@ -5,9 +5,11 @@ import routing from './github.routes';
 import GitHubController from './github.controller';
 
 import githubUser from './directives/github_user';
+import githubRepos from './directives/github_repos';
 
 export default angular.module('app.github', [uirouter])
     .config(routing)
     .controller('GitHubController', GitHubController)
     .directive('githubUser', githubUser)
+    .directive('githubRepos', githubRepos)
     .name;
